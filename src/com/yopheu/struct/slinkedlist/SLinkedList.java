d1b09e9a820579b138c8a081b2a599feafdfef0f
humi53 <j.k.humi9@gmail.com>
2023-07-13 14:46:20 +0900

    sLinkedList핵심기능구현

--- src/com/yopheu/struct/slinkedlist/SLinkedList.java
+++ src/com/yopheu/struct/slinkedlist/SLinkedList.java
@@ -79,65 +79,147 @@ public class SLinkedList<E> implements List<E> {
 		size++;
 	}
 
-	@Override
-	public E remove(int index) {
+	public E remove() {
 		Node<E> headNode = head;
 		if(headNode == null)
 			throw new NoSuchElementException();
+		
 		E element = headNode.data;
 		
 		Node<E> nextNode = head.next;
 		
 		head.data = null;
 		head.next = null;
-		return null;
+		
+		head = nextNode;
+		size--;
+		
+		if(size == 0) {
+			tail = null;
+		}
+		return element;
+	}
+	
+	@Override
+	public E remove(int index) {
+		if(index == 0) {
+			return remove();
+		}
+		if(index >= size || index < 0) {
+			throw new IndexOutOfBoundsException();
+		}
+		
+		Node<E> prevNode = search(index - 1);	// 삭제할 노드의 이전 노드
+		Node<E> removeNode = prevNode.next;		// 삭제할 노드
+		Node<E> nextNode = removeNode.next;		// 삭제할 노드의 다음 노드
+		
+		E element = removeNode.data; 	// 삭제할 노드의 데이터 반환준비
+		
+		// 이전 노드가 가리키는 노드를 삭제하려는 다음 노드로 변경
+		prevNode.next = nextNode;
+		
+		// 만약 삭제했던 노드가 마지막 노드라면 tail 을 prevNode로 갱신
+		if(prevNode.next == null) {
+			tail = prevNode;
+		}
+		
+		// 데이터 삭제
+		removeNode.next = null;
+		removeNode.data = null;
+		size--;
+		
+		return element;
 	}
 	@Override
 	public boolean remove(Object value) {
-		// TODO Auto-generated method stub
-		return false;
+		Node<E> prevNode = head;
+		boolean hasValue = false;	// ? 사용하려다 사용 못한듯....
+		Node<E> x = head;	// removedNode 탐색변수
+
+		// value 와 일치하는 노드를 찾는다
+		for(; x != null; x = x.next) {
+			if(value.equals(x.data)) {
+				hasValue = true;
+				break;
+			}
+			prevNode = x;
+		}
+		
+		// 일치하는 요소가 없을 경우 false 반환
+		if(x == null) {
+			return false;
+		}
+		
+		// 삭제하려는 노드가 head라면 기존 remove()를 사용
+		if(x.equals(head)) {
+			remove();
+			return true;
+		} else {
+			// 이전 노드의 링크를 삭제하려는 노드의 다음 노드로 연결
+			prevNode.next = x.next;
+			
+			// 만약 삭제했던 노드가 마지막 노드라면 tail을 prevNode로 갱신
+			if(prevNode.next == null) {
+				tail = prevNode;
+			}
+			x.data = null;
+			x.next = null;
+			size--;
+			return true;
+		}
 	}
 
 	@Override
 	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return search(index).data;
 	}
 
 	@Override
 	public void set(int index, E value) {
-		// TODO Auto-generated method stub
-
+		Node<E> replaceNode = search(index);
+		replaceNode.data = null;
+		replaceNode.data = value;
 	}
 
 	@Override
 	public boolean contains(Object value) {
-		// TODO Auto-generated method stub
-		return false;
+		return indexOf(value) >= 0;
 	}
 
 	@Override
 	public int indexOf(Object value) {
-		// TODO Auto-generated method stub
-		return 0;
+		int index = 0;
+		
+		for(Node<E> x = head; x != null; x = x.next) {
+			if(value.equals(x.data)) {
+				return index;
+			}
+			index++;
+		}
+		// 찾고자 하는 요소를 찾지 못했을 경우 -1 반환
+		return -1;
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return size == 0;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
-
+		for(Node<E> x = head; x != null;) {
+			Node<E> nextNode = x.next;
+			x.data = null;
+			x.next = null;
+			x = nextNode;
+		}
+		head = tail = null;
+		size = 0;
 	}
 
 }
